2a31f3dc3be295e4643e58eeb1947ce26788b2ba
gingin77 <ginniehench@gmail.com>
2017-09-19 15:34:31 -0400

    the db migration conflict has been resolved. There was a conflict created because the migration record in the migrate dir did NOT match the record in the db > schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919165519) do
+ActiveRecord::Schema.define(version: 20170919185808) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "URL"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
