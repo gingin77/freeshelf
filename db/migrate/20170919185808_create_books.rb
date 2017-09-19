1b6aa495a382896bcb751195940ba4e41c9d867e
gingin77 <ginniehench@gmail.com>
2017-09-19 15:10:24 -0400

    error message says Migrations are pending. To resolve this issue, run: bin/rails db:migrate RAILS_ENV=development

--- db/migrate/20170919185808_create_books.rb
+++ db/migrate/20170919185808_create_books.rb
@@ -1,6 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.text :description
+      t.string :URL
 
       t.timestamps
     end
