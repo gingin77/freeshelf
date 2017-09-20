cb27e423ab49f424092c11080e3905e903c6f03c
gingin77 <ginniehench@gmail.com>
2017-09-20 17:15:37 -0400

    the new user form routes to the books index page now. However, I haven't checked if I can find the new user in the db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to books_path
+    else
+      render 'new'
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+
+
+# See Listing 7.28
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920190652) do
+ActiveRecord::Schema.define(version: 20170920210534) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170920190652) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
+    t.string "password_confirmation"
   end
 
 end
