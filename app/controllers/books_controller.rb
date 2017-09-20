98a404211084b5be20010e0ff972ec505b5b46fa
gingin77 <ginniehench@gmail.com>
2017-09-20 12:31:06 -0400

    I just created a Users Controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,7 +69,6 @@ class BooksController < ApplicationController
       @book = Book.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
       params.require(:book).permit(:id, :title, :author, :description, :URL)
     end
