4f73a4c2e7b86e1c67d0da99e086b4809f86abde
gingin77 <ginniehench@gmail.com>
2017-09-24 22:37:25 -0400

    the user model contains a field for password_digest but not for pw or pw_confirmation, so I removed pw_confirmation from the sign-up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925005922) do
+ActiveRecord::Schema.define(version: 20170925015153) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 20170925005922) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password"
-    t.string "password_confirmation"
     t.string "password_digest"
   end
 
