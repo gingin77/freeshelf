8bd8716567a95530cd74e97cc3bcd467564c61a2
gingin77 <ginniehench@gmail.com>
2017-09-24 23:02:38 -0400

    pw validation is working. PW's must be 6 or more characters. Also, I brought back the pw confirmation field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to books_path
+      redirect_to show
     else
       render 'new'
     end
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
 
 
